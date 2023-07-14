a07d4be65fe086ca426ac4144d00633d1b9fbb3e
kesava <chkesava54@gmail.com>
2023-07-14 05:38:30 +0000

    Added new code

--- app.js
+++ app.js
@@ -29,14 +29,49 @@ app.get("/players/", async (request, response) => {
 });
 app.use(express.json());
 app.post("/players/", async (request, response) => {
-  const bookDetails = request.body;
-  const { player_name, jersey_number, role } = bookDetails;
-  let addDataQuery = ` insert into cricket_team (playerName,jerseyNumber,role) values(
-      ${player_name},
-      ${jersey_number},
-      ${role}
+  const playerDetails = request.body;
+  const { playerName, jerseyNumber, role } = playerDetails;
+  let addDataQuery = ` insert into cricket_team (player_name,jersey_number,role) values(
+      '${playerName}',
+      ${jerseyNumber},
+      '${role}'
   )`;
   const dbResponse = await db.run(addDataQuery);
   const bookId = dbResponse.lastID;
-  response.send({ bookId: bookId });
+  response.send("Player Added To Team");
+});
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  //console.log(playerId);
+  const get_data_query = `
+  select * from cricket_team where player_id =${playerId};
+  `;
+  let book = await db.get(get_data_query);
+  response.send(book);
+});
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const playerDetails = request.body;
+  const { playerName, jerseyNumber, role } = playerDetails;
+  const updateBookQuery = `
+    UPDATE
+      cricket_team
+    SET
+      player_name='${playerName}',
+      jersey_number=${jerseyNumber},
+      role='${role}'
+    WHERE
+      player_id = ${playerId};`;
+  await db.run(updateBookQuery);
+  response.send("Player Details Updated");
+});
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerQuery = `
+    DELETE FROM
+      cricket_team
+    WHERE
+      player_id = ${playerId};`;
+  await db.run(deletePlayerQuery);
+  response.send("Player Removed");
 });
